3473e23e759542f646aedd0c64bf3d1638f1211d
Kirill Vasilenko <wetanos@gmail.com>
2022-06-20 21:46:13 +0300

    feat: added figure's creation feature from the console

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 
 public class Main {
-    public static void main(String[] args) {
-        Square square = new Square(4);
-        square.squareResult();
-        Rectangle rectangle = new Rectangle(5, 7);
-        rectangle.squareResult();
-        Triangle triangle = new Triangle(5, 4, 6);
-        triangle.squareResult();
-        Circle circle = new Circle(4);
-        circle.squareResult();
+    public static void main(String[] args) throws Exception {
+        FigureCreator figureCreator = new FigureCreator();
+        Figures figureToTest = figureCreator.figureCreator();
+        figureToTest.squareResult();
     }
 }
